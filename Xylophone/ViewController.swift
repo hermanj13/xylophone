f2d2bdee44f6947804228e9523b77d463ae8b4d3
hermanj13 <hermanj13@me.com>
2018-01-11 12:13:50 -0500

    simplified controller

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,30 +18,7 @@ class ViewController: UIViewController{
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
-        print(sender.tag)
-        switch sender.tag {
-        case 1:
-            playSound(tag:1)
-        case 2:
-            playSound(tag:2)
-        case 3:
-            playSound(tag:3)
-        case 4:
-            playSound(tag:4)
-        case 5:
-            playSound(tag:5)
-        case 6:
-            playSound(tag:6)
-        case 7:
-            playSound(tag:7)
-        default:
-            print("error")
-        }
-    }
-    
-    
-    func playSound(tag:Int) {
-        guard let url = Bundle.main.url(forResource: "note\(tag)", withExtension: "wav") else { return }
+        guard let url = Bundle.main.url(forResource: "note\(sender.tag)", withExtension: "wav") else { return }
         
         do {
             try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
